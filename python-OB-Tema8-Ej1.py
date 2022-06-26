494f355b8b468a6b497c3a66bc724d0c2be10f0f
unknown <ana.vazquez.s@gmail.com>
2022-06-26 01:45:15 -0300

    Primer commit Ana

---
+++ python-OB-Tema8-Ej1.py
@@ -0,0 +1,8 @@
+# f = open("archivoTexto.txt", "x")
+
+#f = open("archivoTexto.txt", "w")
+
+#f.writelines("TITULO: Ejemplo de manejo de archivos en Python\n", "Este es el primer parrafo.\n", "Este es el segundo parrafo.\n", "Este es el ultimo parrafo.\n")
+
+f = open("archivoTexto.txt", "r")
+print(f.read())
